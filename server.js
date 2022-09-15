bf6189baf14a91df43e74ac14b7939cfff0a4fb1
Amit Kumar <amit.kumar@wingify.com>
2022-09-15 18:26:26 +0530

    html sending

--- server.js
+++ server.js
@@ -1,46 +1,33 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/status/:statusCode", (req, res) => {
+app.use("/:statusCode", (req, res) => {
   let statusToSend = Number(req.params["statusCode"]) || 500;
 
-  let sendData = "Server Error";
+  let sendData = "/serverError.html";
 
   if ([500, 501, 502, 503].includes(statusToSend)) {
     setTimeout(() => {
-      res.status(statusToSend).send(sendData);
-      res.end();
+      res.status(statusToSend);
+      res.sendFile(__dirname + sendData);
     }, 10000);
   } else {
     if ([200, 201, 202, 203].includes(statusToSend)) {
-      sendData = "Loaded Successfully";
+      sendData = "/success.html";
     } else if (statusToSend === 404) {
-      sendData = "Not Found";
+      sendData = "/notFound.html";
     } else if ([400, 401, 402, 403].includes(statusToSend)) {
-      sendData = "Bad Request";
+      sendData = "/badRequest.html";
     } else {
-      sendData = "Error";
+      sendData = "/error.html";
     }
-    res.status(statusToSend).send(sendData);
-    res.end();
+    res.status(statusToSend);
+    res.sendFile(__dirname + sendData);
   }
 });
 
-// app.use(function (err, req, res, next) {
-//   console.log(err.stack);
-//   res.type("text/plain");
-//   res.status(500);
-//   res.send("500 Server Error");
-// });
-
 const port = process.env.PORT || 8000;
+
 app.listen(port, () => {
   console.log(`Server Running on ${port}`);
 });
